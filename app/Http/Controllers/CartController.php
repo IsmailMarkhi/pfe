749797c09bf82b7a4b427ff2a258604dcd56f550
ismail markhi <ismailmarkhi17@gmail.com>
2026-06-11 09:36:02 +0100

    done

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -20,39 +20,58 @@ class CartController extends Controller
         ]);
     }
 
-    public function store(Product $product)
-    {
-        $item = CartItem::where('product_id', $product->id)
-            ->where('user_id', auth()->id())
-            ->first();
 
-        if ($item) {
-            $item->increment('quantity');
-        } else {
-            CartItem::create([
-                'product_id' => $product->id,
-                'user_id' => auth()->id(),
-                'quantity' => 1
-            ]);
+    public function store(Product $product)
+{
+    $item = CartItem::where('product_id', $product->id)
+        ->where('user_id', auth()->id())
+        ->first();
+
+    if ($item) {
+
+       
+        if ($item->quantity >= $product->stock) {
+            return redirect('/cart');
         }
 
-        return redirect('/cart');
+        $item->increment('quantity');
+    } else {
+
+      
+        if ($product->stock <= 0) {
+            return redirect('/products');
+        }
+
+        CartItem::create([
+            'product_id' => $product->id,
+            'user_id' => auth()->id(),
+            'quantity' => 1
+        ]);
     }
+
+    return redirect('/cart');
+}
+
 
     public function update(Request $request, CartItem $cartItem)
-    {
-        abort_if($cartItem->user_id !== auth()->id(), 403);
+{
+    abort_if($cartItem->user_id !== auth()->id(), 403);
 
-        $request->validate([
-            'quantity' => 'required|integer|min:1'
-        ]);
+    $request->validate([
+        'quantity' => 'required|integer|min:1'
+    ]);
 
-        $cartItem->update([
-            'quantity' => $request->quantity
-        ]);
-
+    
+    if ($request->quantity > $cartItem->product->stock) {
         return redirect('/cart');
     }
+
+    $cartItem->update([
+        'quantity' => $request->quantity
+    ]);
+
+    return redirect('/cart');
+}
 
     public function destroy(CartItem $cartItem)
     {
